4645f78af364465aa975a2b4162ce0d76e1cffe5
Deepak Sindhwani <dsindhwani55@gmail.com>
2020-10-19 09:36:57 +0530

    Removed redux completely and implement context api

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyAefYJHbqkScug2LsB3EFymg63eqK4QTW0",
+  authDomain: "crwn-db-ds.firebaseapp.com",
+  databaseURL: "https://crwn-db-ds.firebaseio.com",
+  projectId: "crwn-db-ds",
+  storageBucket: "crwn-db-ds.appspot.com",
+  messagingSenderId: "5223557620",
+  appId: "1:5223557620:web:d5c37b7a00bcb6c118f1a1",
+  measurementId: "G-F3GPL60PFN",
 };
 
 firebase.initializeApp(config);
